2501feffe3d6113ec3ecadbb2e0133f4d23a9c54
axzed <waxwc1491224820@gmail.com>
2022-11-11 22:24:00 +0800

    finished activity data trans

--- chapter03/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.kt
+++ chapter03/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.kt
@@ -1,8 +1,10 @@
 package com.example.activitytest
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Button
@@ -18,8 +20,19 @@ class FirstActivity : AppCompatActivity() {
         // 为控件设置监听器
         button1.setOnClickListener {
 //            Toast.makeText(this, "You clicked Button 1", Toast.LENGTH_SHORT).show()
+//            val intent = Intent(this, SecondActivity::class.java)
+//            val intent = Intent(Intent.ACTION_VIEW)
+//            intent.data = Uri.parse("https://www.baidu.com")
+//            startActivity(intent)
+            // Activity之间传递数据
+//            val data = "Hello SecondActivity"
+//            val intent = Intent(this, SecondActivity::class.java)
+//            intent.putExtra("extra_data", data)
+//            startActivity(intent)
+
+            // 返回数据给上一个Activity
             val intent = Intent(this, SecondActivity::class.java)
-            startActivity(intent)
+            startActivityForResult(intent, 1)
         }
     }
 
@@ -36,5 +49,17 @@ class FirstActivity : AppCompatActivity() {
             R.id.remove_item -> Toast.makeText(this, "You clicked remove", Toast.LENGTH_SHORT).show()
         }
         return true
+    }
+
+
+    // 重写以获得SecondActivity回传的数据
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+            1 -> if (resultCode == RESULT_OK) {
+                val returnedData = data?.getStringExtra("data_return")
+                Log.d("FirstActivity", "returned data is $returnedData")
+            }
+        }
     }
 }
--- chapter03/ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.kt
+++ chapter03/ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.kt
@@ -1,11 +1,32 @@
 package com.example.activitytest
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 
 class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.second_layout)
+//        val extraData = intent.getStringExtra("extra_data")
+//        Log.d("SecondActivity", "extra data is $extraData")
+
+        // 传递数据给上一个Activity
+        val button2: Button = findViewById(R.id.button2)
+        button2.setOnClickListener {
+            val intent = Intent()
+            intent.putExtra("data_return", "Hello FirstActivity")
+            setResult(RESULT_OK, intent)
+            finish()
+        }
+    }
+
+    override fun onBackPressed() {
+        val intent = Intent()
+        intent.putExtra("data_return", "Hello FirstActivity")
+        setResult(RESULT_OK, intent)
+        finish()
     }
 }
