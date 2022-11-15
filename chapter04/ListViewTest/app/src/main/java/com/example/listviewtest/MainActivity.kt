7d61c0ac5dc78271aef69f77d09170abaa8b726a
axzed <waxwc1491224820@gmail.com>
2022-11-15 11:35:10 +0800

    add ListView

--- chapter04/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.kt
+++ chapter04/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.listviewtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class MainActivity : AppCompatActivity() {
+
+    private val data = listOf<String>("Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry",
+    "Mango", "Apple", "Banana", "Orange", "Watermelon", "Pear", "Grape", "Pineapple", "Strawberry", "Cherry", "Mango")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data)
+        val listView: ListView = findViewById(R.id.listView)
+        listView.adapter = adapter
     }
 }
